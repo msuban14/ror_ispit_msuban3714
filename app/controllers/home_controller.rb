ead1419401ec1a7a0afb9bc3079eef5a2622a3b0
Mariano <msuban14@raf.rs>
2021-02-09 17:24:21 +0100

    added little bit of style and changed home controller to redirect if logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
+  before_action :check_signed_in
+
   def index
+  end
+
+  def check_signed_in
+    redirect_to fishing_areas_path if signed_in?
   end
 end
